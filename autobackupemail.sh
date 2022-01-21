b3db83e34991f144fbab781894176025769f8d8e
Gashak22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-21 23:04:55 +0700

    Update autobackupemail.sh

--- autobackupemail.sh
+++ autobackupemail.sh
@@ -4,7 +4,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/Apeachsan91/server/main/ipallow | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Gashakgeneration22/Jojo-vpn/main/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
