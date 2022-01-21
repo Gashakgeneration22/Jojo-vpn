2d18539071f348c47888626bbad91c43631d0079
Gashak22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-21 21:21:42 +0700

    Update add-ssr.sh

--- add-ssr.sh
+++ add-ssr.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-echo "Script By geo"
+echo "Script By JOESATRIA22"
 clear
 IP=$(wget -qO- icanhazip.com);
 echo "Please enter the username you want to set (do not repeat, does not support Chinese, will be reported incorrect!)"
@@ -69,5 +69,5 @@ echo -e "==========================="
 echo -e " Link SSR      : ${ssr_link}"
 echo -e " ==================================================="
 echo -e ""
-echo -e "Script By @sampiiiiu"
+echo -e "Script By @JOESATRIA22"
 echo -e ""
