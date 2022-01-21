2412324108b39b2ce0aa891ee5463a74d17b5756
Gashak22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-21 21:14:41 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -7,6 +7,6 @@ echo -e "#-----------------------------------------------#"| lolcat
 echo -e "# For Debian 9 & Debian 10 64 bit               #"| lolcat
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"| lolcat
 echo -e "# For VPS with KVM and VMWare virtualization    #"| lolcat
-echo -e "# Build Up By geo                               #"| lolcat
+echo -e "# Build Up By JOESATRIA22                       #"| lolcat
 echo -e "#-----------------------------------------------#"| lolcat
 echo -e "================================================="| lolcat
