15de4ed6a47a9259f3fd1f237ffcf916648a32e2
Gashak22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-22 11:54:07 +0700

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -15,7 +15,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://halucok.me/vpn.zip
+wget https://raw.githubusercontent.com/Gashakgeneration22/Jojo-vpn/main/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
