abc1b4e0ce2237af2bb284905d6c4996f2d93a8e
Gashak22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-22 14:02:24 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -8,9 +8,9 @@ clear
 apt install jq curl -y
 DOMAIN=geoganteng.my.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
-SUB_DOMAIN=${sub}.geoganteng.my.id
-CF_ID=paoandest@gmail.com
-CF_KEY=1d158d0efc4eef787222cefff0b6d20981462
+SUB_DOMAIN=${sub}.gashak22.online
+CF_ID=sundaempire03@gmail.com
+CF_KEY=b85ac2f73c9749ad85cc6450fd067c8af5e6c
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${SUB_DOMAIN}..."
