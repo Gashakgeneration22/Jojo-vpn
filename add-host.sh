3befda5d96b263f777752d9cae78cdba93581f9b
Gashak22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-24 23:44:48 +0700

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
+RED='\e[1;31m'
+GREEN='\e[0;32m'
+BLUE='\e[0;34m'
+NC='\e[0m'
+izin
+#Input Domain
+clear
+echo ""
+echo -e "${BLUE}==========================================================${NC}"
+figlet Add Domain 
+echo -e "${BLUE}==========================================================${NC}"
+echo ""
+echo "Please Input Your Pointing Domain In Cloudflare "
+read -rp "Domain/Host: " -e host
+echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
+rm -f /home/homain
+echo "$host" > /home/domain
+echo -e "[${GREEN}Done${NC}]"
 
-read -p "Input New Domain : " domainbaru
+#Update Sertificate SSL
+echo "Automatical Update Your Sertificate SSL"
+sleep 3
+echo Starting Update SSL Sertificate
+sleep 0.5
+source /var/lib/premium-script/ipvps.conf
+domain=$IP
+systemctl stop v2ray
+systemctl stop v2ray@none
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+systemctl start v2ray
+systemctl start v2ray@none
 
-#Validate
-if [[ $domainbaru == "" ]]; then
-echo "Please Input New Domain"
-exit 1
-fi
-
-#Input To Domain
-cat > /etc/v2ray/domain << END
-$domainbaru
-END
-
-clear 
-echo "SUCCESS"
+#Done
+echo -e "[${GREEN}Done${NC}]"
+echo "Location Your Domain : /home/domain"
+echo ""
+echo -e "${RED}Autoscript By JOESATRIA22${NC}"
