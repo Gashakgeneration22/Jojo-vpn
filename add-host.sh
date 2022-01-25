9d00d17a63bfaa0d11ccbc4be9b37be1d2203c4d
Gashak22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-01-25 16:06:02 +0700

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -15,7 +15,9 @@ echo "Please Input Your Pointing Domain In Cloudflare "
 read -rp "Domain/Host: " -e host
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 rm -f /etc/v2ray/domain
-echo "$host" > /etc/v2ray/domain
+cat > /etc/v2ray/domain << END
+$host
+END
 echo -e "[${GREEN}Done${NC}]"
 
 #Update Sertificate SSL
