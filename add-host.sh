9c2faced2a19d232eba703a57ea63dec669141a6
Gashak22 <97940348+Gashakgeneration22@users.noreply.github.com>
2022-03-17 13:45:52 +0700

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 RED='\e[1;31m'
 GREEN='\e[0;32m'
@@ -18,6 +19,19 @@ rm -f /home/domain
 echo "$host" > /home/domain
 echo -e "[${GREEN}Done${NC}]"
 
+#Update Sertificate SSL
+echo "Automatical Update Your Sertificate SSL"
+sleep 3
+echo Starting Update SSL Sertificate
+sleep 0.5
+source /var/lib/premium-script/ipvps.conf
+domain=$IP
+systemctl stop v2ray
+systemctl stop v2ray@none
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+systemctl start v2ray
+systemctl start v2ray@none
 
 #Done
 echo -e "[${GREEN}Done${NC}]"
